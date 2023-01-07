2dbc8ccbf559995a497f8f56029f075b12294663
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-07 15:37:31 +0000

    Fix state manager module (#1)
    * Fixed stateManager
    
    * Clean up
    
    Co-authored-by: Konstantin Milchev <konstantin.milchev@agepartnership.com>

--- src/StateManager.jsx
+++ src/StateManager.jsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
 import { getAPI } from "./AxiosCall";
 
-const useStateManager = (props) => {
-
-const [userName, setUserName] = useState('');
-const [error, setError] = useState('')
+const StateManager = () => {
+  const [userName, setUserName] = useState("");
+  const [error, setError] = useState("");
 
   const changeEvent = (e) => {
-    setUserName(e.target.value)
+    setUserName(e.target.value);
   };
 
-  const onClickEvent = async (e) => {
+  const onClickEvent = async (e, a) => {
+    console.log("onClickEventProps");
+    console.dir(a);
     try {
       e.preventDefault();
       const response = await getAPI(userName);
-      console.dir(response)
-        props.onSubmit(response.data)
-        setError(null)
-        setUserName('')
-      
+      a.onSubmit(response.data);
+      setError(null);
+      setUserName("");
     } catch (error) {
-      setError(error)
-      console.log(error)
-    }  
-  }
-}
+      setError(error);
+      console.log(error);
+    }
+  };
 
-export default useStateManager;
+  return { userName, changeEvent, onClickEvent };
+};
+export default StateManager;
 // export {changeEvent, onClickEvent}
